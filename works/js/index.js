8200a87232a7ce9fd44c4a229ab99da24ee1f881
szybj <songzhiyin@outlook.com>
2017-01-18 02:14:29 +0800

    移动端bug

--- works/js/index.js
+++ works/js/index.js
@@ -2,7 +2,7 @@
     * @Author: szy
     * @Date:   2016-12-29 01:51:31
     * @Last Modified by:   szybj
-    * @Last Modified time: 2017-01-16 23:53:21
+    * @Last Modified time: 2017-01-18 02:04:00
     *
    'use strict';
 
@@ -138,6 +138,9 @@
             var clientWidth = $(window).width();
             console.log(clientWidth)
             if(clientWidth > 1021){
+                $('.seaF').css({
+                       'height':'100px'
+                   })
                 $('.wrap').css({
                     'width':'925px',
                     'marginLeft': '-462px'
