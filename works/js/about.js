8783efd5e6bb2559bc1adc53701e3a3b695c742d
szybj <songzhiyin@outlook.com>
2017-02-06 14:09:49 +0800

    face

--- works/js/about.js
+++ works/js/about.js
@@ -2,7 +2,7 @@
  * @Author: szybj
  * @Date:   2017-02-04 13:38:32
  * @Last Modified by:   szybj
- * @Last Modified time: 2017-02-05 12:24:07
+ * @Last Modified time: 2017-02-06 13:56:51
  */
 
 'use strict';
@@ -10,10 +10,12 @@
 
     var timer = null;
     var n = 0;
-    var $f = $('.f');
-    init();
-    console.log($f)
-    timer = setInterval(function() {
+    var $f = $('.f'),
+        $facesWrap = $('.facesWrap');
+
+   init();
+
+timer = setInterval(function() {
         init();
     }, 6000);
 
@@ -35,5 +37,32 @@
 
     }
 
+    $facesWrap.on('mouseover',function(ev){
+        var iX = ev.clientX - (getX(this)+this.offsetWidth/2),
+            iY = ev.clientY - (getY(this)+this.offsetHeight/2);
+     /*   var iY = parseInt(ev.clientY - getY(this));*/
+        this.style.transform = 'rotateX('+iY*0.15+'deg) rotateY('+iX*0.15+'deg)'
+     document.title = iX;
+
+
+
+    });
+
+    function getX(obj){
+        var iLeft = 0;
+        while(obj){
+            iLeft += obj.offsetLeft;
+            obj = obj.offsetParent;
+        }
+        return iLeft;
+    }
+   function getY(obj){
+        var iTop = 0;
+        while(obj){
+            iTop += obj.offsetTop;
+            obj = obj.offsetParent;
+        }
+        return iTop;
+    }
 
 })();
