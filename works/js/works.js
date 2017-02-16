057c9011b390668458bd2bfdc9104fe12d2c0854
szybj <songzhiyin@outlook.com>
2017-02-16 19:05:46 +0800

    link

--- works/js/works.js
+++ works/js/works.js
@@ -2,14 +2,14 @@
  * @Author: szybj
  * @Date:   2017-02-04 13:38:32
  * @Last Modified by:   szy
- * @Last Modified time: 2017-02-16 16:37:08
+ * @Last Modified time: 2017-02-16 19:04:43
  */
 
 'use strict';
 (function() {
-    var data = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg','images/5.jpg','images/6.jpg'],
+    var data = ['images/4.jpg','images/5.jpg','images/6.jpg', 'images/1.jpg', 'images/2.jpg', 'images/3.jpg'],
         titleData = ['外卖app(vue)','贪吃蛇','下雪效果','css3网页平滑过渡效果', '评论功能', '在线教育'],
-        linkDate = ['https://szybj.github.io/css3%E7%BD%91%E9%A1%B5%E5%B9%B3%E6%BB%91%E8%BF%87%E6%B8%A1%E6%95%88%E6%9E%9C/index.html', 'https://szybj.github.io/rr/index.html', 'https://szybj.github.io/e-Learning/', 'https://szybj.github.io/jd/index.html','https://szybj.github.io/%E8%B4%AA%E5%90%83%E8%9B%87/index.html','https://szybj.github.io/%E4%B8%8B%E9%9B%AA%E6%95%88%E6%9E%9C/index.html']
+        linkDate = [ 'hhttps://szybj.github.io/elm/index.html','https://szybj.github.io/%E8%B4%AA%E5%90%83%E8%9B%87/index.html','https://szybj.github.io/%E4%B8%8B%E9%9B%AA%E6%95%88%E6%9E%9C/index.html','https://szybj.github.io/css3%E7%BD%91%E9%A1%B5%E5%B9%B3%E6%BB%91%E8%BF%87%E6%B8%A1%E6%95%88%E6%9E%9C/index.html', 'https://szybj.github.io/rr/index.html', 'https://szybj.github.io/e-Learning/']
     var str = '';
     var timer = null;
     var n = 0;
